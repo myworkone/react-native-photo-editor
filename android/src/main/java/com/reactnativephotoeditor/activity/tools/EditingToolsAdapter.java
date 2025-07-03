9ab53ff85d7894fa125c350b4418bdb80ee94eff
Nicolas Marin <nicolas.marin@crifa.com>
2025-07-03 15:53:34 -0500

    AF-3384/android side working

--- android/src/main/java/com/reactnativephotoeditor/activity/tools/EditingToolsAdapter.java
+++ android/src/main/java/com/reactnativephotoeditor/activity/tools/EditingToolsAdapter.java
@@ -27,9 +27,9 @@ public class EditingToolsAdapter extends RecyclerView.Adapter<EditingToolsAdapte
   public EditingToolsAdapter(OnItemSelected onItemSelected) {
     mOnItemSelected = onItemSelected;
     mToolList.add(new ToolModel("Shape", R.drawable.ic_brush, ToolType.SHAPE));
-    mToolList.add(new ToolModel("Eraser", R.drawable.ic_eraser, ToolType.ERASER));
-    mToolList.add(new ToolModel("Filter", R.drawable.ic_colorfilter, ToolType.FILTER));
-    mToolList.add(new ToolModel("Sticker", R.drawable.ic_sticker, ToolType.STICKER));
+    // mToolList.add(new ToolModel("Eraser", R.drawable.ic_eraser, ToolType.ERASER));
+    // mToolList.add(new ToolModel("Filter", R.drawable.ic_colorfilter, ToolType.FILTER));
+    // mToolList.add(new ToolModel("Sticker", R.drawable.ic_sticker, ToolType.STICKER));
     mToolList.add(new ToolModel("Text", R.drawable.ic_smallcaps, ToolType.TEXT));
   }
 
